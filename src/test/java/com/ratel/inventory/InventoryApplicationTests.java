96b38cc3943f85d43da5c73bd9d314650819a26f
Francis Kangaty <franciskangaty@gmail.com>
2025-12-28 13:57:41 +0300

    first commit

---
+++ src/test/java/com/ratel/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratel.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
